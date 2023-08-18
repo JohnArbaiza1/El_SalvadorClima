3799efac9099ac35e077ae23cc5676e8f5840009
John Elvis Arbaiza Velásquez <102486877+JohnArbaiza1@users.noreply.github.com>
2023-08-17 20:52:33 -0600

    Actualización

--- Clima_ESV.js
+++ Clima_ESV.js
@@ -97,3 +97,24 @@ const departamentoKey = departamentosMap['Morazán'];
 if (departamentoKey) {
     El_Salvador().ClimaDepartamentos[departamentoKey]();
 }
+
+//------------------------------------------------------------------
+//Aca se maneja el cambio del tema de la pagina
+//Const a emplear
+const CambioTema = document.getElementById('Tema');
+const body = document.body;
+
+//Evento que se encargara del cambio 
+CambioTema.addEventListener("click", () =>{
+    body.classList.toggle("Temas");
+
+    //Cambiamos los iconos segun el modo en que se encuentre actualmente la pagina
+    const sunIcon = '<i class="fas fa-sun"></i>';
+    const moonIcon = '<i class="fas fa-moon"></i>';
+
+    if (body.classList.contains("Temas")) {
+        toggleButton.innerHTML = sunIcon; // Cambiar a icono de sol (modo día)
+      } else {
+        toggleButton.innerHTML = moonIcon; // Cambiar a icono de luna (modo noche)
+      }
+});
